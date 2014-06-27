63131858cc277eef0fa8a48a316567c5d7754c3a
violentninja <d.fern@hotmail.co.uk>
2014-06-27 19:46:18 +0100

    Mod actually loads in Minecraft :) Not much happens though :(

---
+++ src/main/java/violentninjad/violentmod/ViolentMod.java
@@ -0,0 +1,9 @@
+package violentninjad.violentmod;
+//Copyright ViolentNInjaD 2014
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid="ViolentMod", name="Violent Mod", version="AlphaBuild1")
+public class ViolentMod {
+
+}
