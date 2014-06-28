28244638ccc2dda8cc67801b28843191478214c3
violentninja <d.fern@hotmail.co.uk>
2014-06-28 18:34:58 +0100

    More file/directory structure and my first item :) No texture yet though.

---
+++ src/main/java/violentninjad/violentmod/item/ItemBlueCrystal.java
@@ -0,0 +1,12 @@
+package violentninjad.violentmod.item;
+
+import violentninjad.violentmod.reference.ModInfo;
+
+public class ItemBlueCrystal extends ItemViolentMod {
+	
+	public ItemBlueCrystal() {
+		super();
+		this.setUnlocalizedName("blueCrystal");
+	}
+
+}
